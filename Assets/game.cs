70e97923f27c331ab1486ac418460a90491eb9be
Kim Falck <38192323+sloppydemon@users.noreply.github.com>
2023-11-20 18:43:19 +0100

    toon testing

---
+++ Assets/game.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class game : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        QualitySettings.vSyncCount = 10;
+        Application.targetFrameRate = 15;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
