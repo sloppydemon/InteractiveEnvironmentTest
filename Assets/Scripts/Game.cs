ee8006668d0f6062f7a42105084d3cbbb101a2a8
Kim Falck <38192323+sloppydemon@users.noreply.github.com>
2023-12-04 17:00:58 +0100

    added cursors

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,9 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public Camera idCamera;
+    public TextMeshPro idText;
+    public Material idMaterial;
+    public GameObject idPrefab;
+    public GameObject cursorTalk;
+    public GameObject cursorPush;
+    public GameObject cursorGrab;
+    public GameObject cursorTake;
+    public GameObject cursorMount;
+
     void Start()
     {
         QualitySettings.vSyncCount = 10;
--- Assets/Scripts/Keywords.cs
+++ Assets/Scripts/Keywords.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.Entities;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,4 +13,112 @@ public class Keywords : MonoBehaviour
     public bool canTake;
     public bool canGrab;
     public bool canPush;
+    public bool canMount;
+    public bool canCommunicate;
+    public string displayName;
+    public string killerName;
+    public Vector3 idScale;
+    public Vector3 idOffset;
+    
+    Camera cam;
+    Game game;
+    GameObject idCube;
+    GameObject idCursor;
+    bool id;
+    Ray ray;
+    RaycastHit hitInfo;
+    bool hit;
+
+    void Start()
+    {
+        cam = Camera.main;
+        game = GameObject.FindGameObjectWithTag("GameController").GetComponent<Game>();
+        idCube = null;
+        id = false;
+        idCursor = null;
+    }
+
+    private void OnMouseEnter()
+    {
+        if (Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt))
+        {
+            if (!id)
+            {
+                Identify();
+            }
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt))
+        {
+            if (id)
+            {
+                idCursor.transform.position = RayCaster() + (0.02f * hitInfo.normal);
+                idCursor.transform.rotation = Quaternion.Euler(-ray.direction);
+            }
+            else
+            {
+                Identify();
+            }
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (id)
+        {
+            Destroy(idCursor);
+            idCursor = null;
+            Destroy(idCube);
+            idCube = null;
+            id = false;
+        }
+    }
+
+    void Identify()
+    {
+        game.idText.text = displayName;
+        game.GetComponent<Camera>().Render();
+        id = true;
+        GameObject idCursorSelect = null;
+        if (canCommunicate)
+        {
+            idCursorSelect = game.cursorTalk;
+        }
+        else if (canTake)
+        {
+            idCursorSelect = game.cursorTake;
+        }
+        else if (canGrab)
+        {
+            idCursorSelect = game.cursorGrab;
+        }
+        else if (canPush)
+        {
+            idCursorSelect = game.cursorPush;
+        }
+        else if (canMount)
+        {
+            idCursorSelect = game.cursorMount;
+        }
+        idCursor = Instantiate(idCursorSelect, RayCaster() + (0.02f * hitInfo.normal), Quaternion.Euler(-ray.direction), game.transform);
+        idCube = Instantiate(game.idPrefab, idCursor.transform.position + (0.01f * Vector3.up), idCursor.transform.rotation, game.transform);
+    }
+
+    public Vector3 RayCaster()
+    {
+        hitInfo = new RaycastHit();
+        ray = cam.ScreenPointToRay(Input.mousePosition);
+        hit = Physics.Raycast(ray, out hitInfo, 15f);
+        if (hit)
+        {
+            return(hitInfo.point);
+        }
+        else
+        {
+            return(Vector3.zero);
+        }
+    }
 }
